85b01ef0cd784756b4a39902da720c82e41c5d04
Little_Soul <2833896014@qq.com>
2021-10-27 16:01:49 +0800

    Update

---
+++ docs/scripts/intector.js
@@ -0,0 +1,37 @@
+hexo.extend.injector.register(
+  "body_end",
+  `<script
+        src='https://cdn.bootcdn.net/ajax/libs/canvas-nest.js/2.0.4/canvas-nest.js'
+        color='183,20,147'
+        pointColor='160,0,100'
+        opacity='0.8'
+        zIndex='-1'
+        count='500'
+    ></script>`
+);
+
+hexo.extend.injector.register(
+  "head_end",
+  `<script src='/js/FunnyTitle.js'></script>`
+);
+
+hexo.extend.injector.register(
+  "head_end",
+  `<script src='/live2d-widget/autoload.js'></script>`
+);
+
+hexo.extend.injector.register(
+  "head_end",
+  `<link rel="stylesheet" href='/css/mouse.css'>`
+);
+
+hexo.extend.injector.register(
+  "head_end",
+  `<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css'>
+  <link rel='stylesheet' href='/css/live2d.css'>`
+);
+
+hexo.extend.injector.register(
+  "head_end",
+  `<script src='/js/WaifuMove.js'></script>`
+);
---
+++ docs/source/js/WaifuMove.js
@@ -0,0 +1,17 @@
+window.onload = () => {
+  //元素鼠标按下事件
+  //当鼠标在这个元素身上按下的时候 就会触发回调函数
+  var box = document.getElementById("waifu");
+  document.onmousedown = function (e) {
+    let disx = e.pageX - box.offsetLeft;
+    let disy = e.pageY - box.offsetTop;
+    document.onmousemove = function (e) {
+      box.style.left = e.pageX - disx + "px";
+      box.style.top = e.pageY - disy + "px";
+    };
+    //释放鼠标按钮，将事件清空，否则始终会跟着鼠标移动
+    document.onmouseup = function () {
+      document.onmousemove = document.onmouseup = null;
+    };
+  };
+};
